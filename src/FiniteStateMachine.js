5855e73a71489d1ea291362dc094451f889b7718
Brendan McMahon <bernos@gmail.com>
2015-01-27 20:42:58 +1100

    Refactored

--- src/FiniteStateMachine.js
+++ src/FiniteStateMachine.js
@@ -17,16 +17,134 @@
    *    ]
    * }
    */  
+
   var FSM = function(spec) {
-    this._spec = spec || {};
-    this._currentState = null;
-    this._cancelled = false;
-    this._handlers = {};
+    var spec = spec || {},
+        currentState = null,
+        cancelled = false,
+        handlers = {},
+        self = this;
+
+    function _getInitialState() {
+      return _getState(spec.initial);
+    };
+
+    function _getState(name) {
+      if (null === name || null === spec.states) {
+        return null;
+      }
+
+      for (var i in spec.states) {
+        if (spec.states[i]['name'] === name) {
+          return spec.states[i];
+        }
+      }
+
+      return null;
+    };
+
+    function _transitionTo(state, data) {
+      if (null === state) { return null; }
+      
+      cancelled = false;
+      
+      if (currentState && currentState.exitEvent) {
+        self.trigger(currentState.exitEvent, self, data);
+      }  
+
+      if (cancelled) {
+        cancelled = false;
+        return null;
+      }
+
+      if (state.entryEvent) {
+        self.trigger(state.entryEvent, self, data);
+      }
+
+      if (cancelled) {
+        cancelled = false;
+        return null;
+      }
+
+      currentState = state;
+
+      if (currentState.changeEvent) {
+        self.trigger(currentState.changeEvent, self, data);
+      }
+
+      self.trigger(FSM.CHANGE, self, data);
+    };
+
+    function _getTargetStateForAction(action) {
+      if (null === currentState) { return null; }
+      if (null === currentState.transitions) { return null; }
+
+      for (var i = 0, m = currentState.transitions.length; i < m; i++) {
+        if (currentState.transitions[i]['action'] === action) {
+          return _getState(currentState.transitions[i]['target']);
+        }
+      }
+
+      return null;
+    }
+
+    this.bind = function(event, handler) {
+      (handlers[event] || (handlers[event] = [])).push(handler);
+      return self;
+    };
+
+    this.unbind = function(event, handler) {
+      if (!event) {
+        handlers = {};
+      } else if (!handler) {
+        handlers[event] = [];
+      } else if (handlers[event]) {
+        var l = handlers[event];
+        for(var i = l.length - 1; i > -1; i--) {
+          l.splice(i,1);
+          return self;
+        }
+      }
+      return self;
+    };
+
+    this.trigger = function(event) {
+      var l;
+      if (l = handlers[event]) {
+        for (var i = 0, m = l.length; i < m; i++) {
+          l[i].apply(self, Array.prototype.slice.call(arguments, 1));
+        }
+      }
+      return self;
+    };
+
+    this.run = function() {
+      if (_getInitialState()) {
+        _transitionTo(_getInitialState(), null);
+      }
+    };
+
+    this.doAction = function(action, data) {
+      var data = data || {};
+      var newState = _getTargetStateForAction(action);
+      
+      if (newState) {
+        _transitionTo(newState, data);
+      }
+    };
+
+    this.cancel = function() {
+      cancelled = true;
+    };
+
+    this.getCurrentState = function() {
+      return currentState;
+    };
   }
 
   FSM.VERSION = '0.0.1';
   FSM.CHANGE  = "FiniteStateMachine:Change";
-
+  /*
   FSM.prototype = {
     bind: function(ev, handler) {
       var l = this._handlers[ev] || (this._handlers[ev] = []);
@@ -148,7 +266,7 @@
 
       return null;
     }
-  }  
+  }*/  
 
   if (global.FiniteStateMachine) {
     throw new Error("FiniteStateMachine has already been defined.");
